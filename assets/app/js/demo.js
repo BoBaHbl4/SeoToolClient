6ad3053b54b112bc587116a3d0d402f586800914
BoBaHbl4 <bobahbl4@gmail.com>
2017-09-25 21:17:06 +0300

    Fixed datatable sortable bag.

--- assets/app/js/demo.js
+++ assets/app/js/demo.js
@@ -1,80 +1,3 @@
-var TableDatatables = function () {
-
-    var initTable = function () {
-        var table = jQuery('.data-table-init');
-
-        table.dataTable({
-
-            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
-            "language": {
-                "aria": {
-                    "sortAscending": ": activate to sort column ascending",
-                    "sortDescending": ": activate to sort column descending"
-                },
-                "emptyTable": "No data available in table",
-                "info": "Showing _START_ to _END_ of _TOTAL_ entries",
-                "infoEmpty": "No entries found",
-                "infoFiltered": "(filtered1 from _MAX_ total entries)",
-                "lengthMenu": "Show _MENU_ entries",
-                "search": "Search:",
-                "zeroRecords": "No matching records found"
-            },
-
-            // Or you can use remote translation file
-            //"language": {
-            //   url: '//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Portuguese.json'
-            //},
-
-            // setup buttons extension: http://datatables.net/extensions/buttons/
-            buttons: [
-                // { extend: 'print', className: 'btn dark btn-outline' },
-                // { extend: 'pdf', className: 'btn green btn-outline' },
-                // { extend: 'csv', className: 'btn purple btn-outline ' }
-            ],
-
-            // scroller extension: http://datatables.net/extensions/scroller/
-            //scrollY:        323,
-            //deferRender:    true,
-            //scroller:       false,
-            //deferRender:    false,
-            //scrollX:        false,
-            //scrollCollapse: false,
-
-            stateSave:      false,
-
-            "order": [
-                //[0, 'asc']
-            ],
-
-            "lengthMenu": [
-                [10, 15, 20, -1],
-                [10, 15, 20, "All"] // change per page values here
-            ],
-            // set the initial value
-            "pageLength": 10,
-
-            "pagingType": "bootstrap_extended",
-
-            "dom": "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>"
-        });
-
-    };
-
-    return {
-
-        //main function to initiate the module
-        init: function () {
-
-            if (!jQuery().dataTable) {
-                return;
-            }
-
-            initTable();
-        }
-
-    };
-
-}();
 $(document).ready( function () {
 
     // For demonstration of templates navigation only
@@ -115,16 +38,13 @@ $(document).ready( function () {
         return false;
     });
 
-    // Init standard project data table
-    TableDatatables.init();
-
     // iCheck check all table rows function
     var tableCheckAllTrigger = jQuery('th.check-field input[data-name=icheck]');
     tableCheckAllTrigger.on('ifChecked', function (event) {
-        jQuery('td.check-field input[data-name=icheck]').iCheck('check');
+        $('td.check-field input[data-name=icheck]').iCheck('check');
     });
     tableCheckAllTrigger.on('ifUnchecked', function (event) {
-        jQuery('td.check-field input[data-name=icheck]').iCheck('uncheck');
+        $('td.check-field input[data-name=icheck]').iCheck('uncheck');
     });
 
 });
--- assets/snippets/pages/datatables-tickets.js
+++ assets/snippets/pages/datatables-tickets.js
@@ -4,13 +4,13 @@ var DatatableTickets = function () {
     // demo initializer
     var tickets = function () {
 
-        var dataJSONArray = JSON.parse('[{"SubjectID":0,"Department":"SocialJuice","Subject":"Laborum amet eu sit irure ex amet mollit laboris consectetur deserunt mollit cillum deserunt. Non aute sunt culpa labore. Tempor elit laborum ex amet ipsum ea Lorem ex culpa ut ex aute sint.","Status":3,"LastUpdated":"2015/04/09"},{"SubjectID":1,"Department":"Content Writing","Subject":"Aliquip non eu non culpa nulla voluptate mollit. Ullamco et nostrud laborum commodo exercitation ad non eu. Fugiat non commodo exercitation pariatur elit dolor fugiat proident magna duis qui dolor ullamco.","Status":3,"LastUpdated":"2015/11/20"},{"SubjectID":2,"Department":"SocialJuice","Subject":"Nisi eiusmod magna Lorem consequat aute ad in labore laboris nisi sint pariatur. Ipsum duis amet velit consectetur et est aute tempor dolore eu proident consequat quis. Magna ut aliqua qui id.","Status":3,"LastUpdated":"2016/05/09"},{"SubjectID":3,"Department":"Content Writing","Subject":"Aute labore est do fugiat. Non duis magna sunt ullamco. Exercitation sit enim ipsum labore sint dolore commodo laborum non ex labore.","Status":1,"LastUpdated":"2014/07/22"},{"SubjectID":4,"Department":"SocialJuice","Subject":"Non ut aliqua reprehenderit nulla tempor nostrud quis velit incididunt aliqua consectetur excepteur culpa. Mollit laborum do magna officia eu qui laboris sint occaecat officia. Id commodo quis laborum culpa.","Status":3,"LastUpdated":"2017/04/02"},{"SubjectID":5,"Department":"SocialJuice","Subject":"Veniam nisi non esse ullamco minim pariatur. Duis id anim esse nostrud quis dolore. Ut quis consectetur est tempor minim ullamco.","Status":3,"LastUpdated":"2014/02/20"},{"SubjectID":6,"Department":"Content Writing","Subject":"Cupidatat laboris dolore dolor officia officia eiusmod aliquip voluptate commodo eu deserunt. Eu magna est ipsum proident amet proident sunt do sit tempor pariatur. Enim ea occaecat pariatur qui cillum laborum velit.","Status":3,"LastUpdated":"2014/10/26"},{"SubjectID":7,"Department":"SocialJuice","Subject":"Id excepteur est laboris non. Laborum pariatur commodo velit consectetur duis. Incididunt aliquip excepteur aliqua Lorem ut enim pariatur sit excepteur aliquip aliquip pariatur aliqua.","Status":1,"LastUpdated":"2016/06/04"},{"SubjectID":8,"Department":"Content Writing","Subject":"Incididunt nostrud reprehenderit fugiat dolor veniam. Consectetur quis sunt incididunt sit eu tempor et officia. Deserunt elit in qui aute consequat nulla dolor.","Status":2,"LastUpdated":"2015/12/27"},{"SubjectID":9,"Department":"Content Writing","Subject":"Reprehenderit reprehenderit occaecat non magna culpa magna qui ad adipisicing ea incididunt eu. Labore velit et qui esse velit. Ipsum ut sit minim laboris exercitation deserunt voluptate cillum nostrud.","Status":2,"LastUpdated":"2016/08/24"},{"SubjectID":10,"Department":"Social Signal","Subject":"Ut occaecat sit cupidatat incididunt aliqua. Sit Lorem eu amet aliquip eiusmod nisi magna aliquip ut occaecat dolor duis velit tempor. Commodo adipisicing excepteur anim in officia sint excepteur sint est ad consequat ullamco.","Status":3,"LastUpdated":"2017/03/18"},{"SubjectID":11,"Department":"Content Writing","Subject":"Ipsum esse aliquip cupidatat reprehenderit irure. Veniam ut adipisicing culpa in laboris et voluptate ut. Qui eu culpa pariatur incididunt sit anim aliquip mollit quis magna veniam id qui voluptate.","Status":3,"LastUpdated":"2017/02/03"},{"SubjectID":12,"Department":"Social Signal","Subject":"Enim eiusmod ipsum exercitation excepteur culpa do labore id incididunt proident consequat id elit veniam. Nulla exercitation adipisicing mollit et incididunt eu sit commodo. Magna labore occaecat velit qui culpa.","Status":1,"LastUpdated":"2016/03/03"},{"SubjectID":13,"Department":"Content Writing","Subject":"Esse ex voluptate Lorem ipsum veniam eu eu ullamco. Minim sint ad est nostrud aute adipisicing labore. Deserunt qui non eiusmod ullamco.","Status":2,"LastUpdated":"2016/07/05"},{"SubjectID":14,"Department":"SocialJuice","Subject":"Aliqua laboris velit nulla do ullamco excepteur cupidatat et consequat. Sunt fugiat Lorem et quis fugiat incididunt cupidatat officia dolor dolor veniam aute. Esse ex anim laborum sint et culpa voluptate consectetur in dolore labore.","Status":1,"LastUpdated":"2016/10/12"},{"SubjectID":15,"Department":"Content Writing","Subject":"Duis quis et incididunt ipsum dolor. Ullamco mollit incididunt qui sint nostrud quis adipisicing velit incididunt. Esse ullamco velit Lorem consequat proident commodo sunt voluptate est et.","Status":2,"LastUpdated":"2014/11/25"},{"SubjectID":16,"Department":"Social Signal","Subject":"Culpa exercitation cillum nisi dolor duis. Cillum do eiusmod nostrud sunt culpa mollit id ad. Irure pariatur voluptate ea cillum laborum qui occaecat proident id.","Status":1,"LastUpdated":"2017/08/12"},{"SubjectID":17,"Department":"SocialJuice","Subject":"Adipisicing anim id laborum excepteur. Quis aliquip dolore occaecat adipisicing voluptate est amet ea. Est excepteur amet sunt commodo deserunt aute.","Status":1,"LastUpdated":"2015/09/19"},{"SubjectID":18,"Department":"Social Signal","Subject":"Cupidatat Lorem exercitation eiusmod exercitation aliquip consequat magna aliqua duis ullamco eu voluptate minim exercitation. Sit ea sunt nulla do dolore aliquip officia. Ut anim ea nulla ipsum.","Status":3,"LastUpdated":"2016/08/03"}]');
+        var ticketsListData = JSON.parse('[{"Department":"SocialJuice","Subject":"Laborum amet eu sit irure ex amet mollit laboris consectetur","Status":3,"LastUpdated":"2015/04/09","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Aliquip non eu non culpa.","Status":3,"LastUpdated":"2015/11/20","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Nisi eiusmod magna Lorem consequat aute","Status":3,"LastUpdated":"2016/05/09","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Aute labore est ","Status":1,"LastUpdated":"2014/07/22","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Non ut aliqua reprehenderit nulla","Status":3,"LastUpdated":"2017/04/02","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Veniam nisi","Status":3,"LastUpdated":"2014/02/20","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Cupidatat laboris dolore dolor officia ","Status":3,"LastUpdated":"2014/10/26","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Id excepteur est laboris non. Laborum pariatur commodo velit consectetur duis.","Status":1,"LastUpdated":"2016/06/04","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Incididunt nostrud reprehenderit fugiat dolor veniam","Status":2,"LastUpdated":"2015/12/27","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Reprehenderit reprehenderit occaecat non magna.","Status":2,"LastUpdated":"2016/08/24","Actions":"ticket_view.html"},{"Department":"Social Signal","Subject":"Ut occaecat sit cupidatat incididunt aliqua","Status":3,"LastUpdated":"2017/03/18","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Ipsum esse aliquip cupidatat reprehenderit irure.","Status":3,"LastUpdated":"2017/02/03","Actions":"ticket_view.html"},{"Department":"Social Signal","Subject":"Enim eiusmod ipsum exercitation excepteur culpa do labore id incididunt proident consequat id.","Status":1,"LastUpdated":"2016/03/03","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Esse ex voluptate Lorem","Status":2,"LastUpdated":"2016/07/05","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Aliqua laboris velit","Status":1,"LastUpdated":"2016/10/12","Actions":"ticket_view.html"},{"Department":"Content Writing","Subject":"Duis quis et incididunt ipsum dolor. Ullamco mollit incididunt qui sint ","Status":2,"LastUpdated":"2014/11/25","Actions":"ticket_view.html"},{"Department":"Social Signal","Subject":"Culpa exercitation.","Status":1,"LastUpdated":"2017/08/12","Actions":"ticket_view.html"},{"Department":"SocialJuice","Subject":"Adipisicing anim id laborum excepteur.","Status":1,"LastUpdated":"2015/09/19","Actions":"ticket_view.html"},{"Department":"Social Signal","Subject":"Cupidatat Lorem exercitation eiusmod exercitation aliquip consequat.","Status":3,"LastUpdated":"2016/08/03","Actions":"ticket_view.html"}]');
 
         var datatable = $('#m_datatable').mDatatable({
             // datasource definition
             data: {
                 type: 'local',
-                source: dataJSONArray,
+                source: ticketsListData,
                 pageSize: 10
             },
 
@@ -19,7 +19,7 @@ var DatatableTickets = function () {
                 theme: 'default', // datatable theme
                 class: 'm_datatable--seognal-tickets', // custom wrapper class
                 scroll: true, // enable/disable datatable scroll both horizontal and vertical when needed.
-                height: '', // datatable's body's fixed height
+                height: null, // datatable's body's fixed height
                 footer: false // display/hide footer
             },
 
@@ -36,21 +36,17 @@ var DatatableTickets = function () {
 
             // columns definition
             columns: [{
-                field: "SubjectID",
-                title: "#",
-                width: 50,
-                sortable: true,
-                selector: false,
-                textAlign: 'center'
-            }, {
                 field: "Department",
-                title: "Department"
+                title: "Department",
+                width: 110
             }, {
                 field: "Subject",
-                title: "Subject"
+                title: "Subject",
+                width: 220
             }, {
                 field: "Status",
                 title: "Status",
+                width: 80,
                 // callback function support for column rendering
                 template: function (row) {
                     var status = {
@@ -62,19 +58,17 @@ var DatatableTickets = function () {
                 }
             }, {
                 field: "LastUpdated",
-                title: "Last Updated"
+                title: "Last Updated",
+                textAlign: 'center',
+                width: 120
             }, {
+                sortable: false,
                 field: "Actions",
+                textAlign: 'center',
                 width: 60,
                 title: "Actions",
-                sortable: false,
-                overflow: 'visible',
-                template: function () {
-                    return '\
-                        <a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="View ">\
-                            <i class="la la-eye"></i>\
-                        </a>\
-					';
+                template: function (row) {
+                    return '<a href="' + row.Actions + '" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="View"><i class="la la-eye"></i></a>';
                 }
             }]
         });
@@ -108,4 +102,5 @@ var DatatableTickets = function () {
 
 jQuery(document).ready(function () {
     DatatableTickets.init();
+    $('#m_datatable table thead th:last-child').removeClass('m-datatable__cell--sort');
 });
